172537823575d69b8d88e96d6ef32dfb7fb8ab98
Sarmat <turiev02@mail.ru>
2024-09-29 11:08:44 +0300

    main logic added

--- src/cpp/src/main.cpp
+++ src/cpp/src/main.cpp
@@ -12,6 +12,9 @@ void clear_dataset1(const Config &config){
     TableWorker tableWorker(config);
     const std::string tableDataset1 = "table_dataset1";
     auto csv = config.get<std::string>("files","csv1","text.txt");
+    if (!file_exists(csv)){
+        write_dataset12(csv,"uid","full_name","email","address","sex","birthdate","phone");
+    }
     std::vector <TableDataset1Data> data = tableWorker.get_normalazied_dataset1(tableDataset1);
     for (const auto& [uid,fullName,email,address,sex,birthdate,phone] : data){
         write_dataset12(csv,uid,fullName,email,address,sex,birthdate,phone);
@@ -23,6 +26,9 @@ void clear_datataset2(const Config &config){
     const std::string tableDataset2 = "table_dataset2";
     std::vector <TableDataset2Data> data = tableWorker.get_normalalized_dataset2(tableDataset2);
     auto csv = config.get<std::string>("files","csv2","text.txt");
+    if (!file_exists(csv)){
+        write_dataset12(csv,"uid","first_name","middle_name","last_name","birthdate","phone","address");
+    }
     for (const auto& [uid,first_name,middle_name,last_name,birthdate,phone,address] : data){
         write_dataset12(csv,uid,first_name,middle_name,last_name,birthdate,phone,address);
     }
@@ -32,6 +38,9 @@ void clear_dataset3(const Config &config){
     TableWorker tableWorker(config);
     const std::string tableDataset3 = "table_dataset3";
     auto csv = config.get<std::string>("files","csv3","text.txt");
+    if (!file_exists(csv)){
+        write_dataset3(csv,"uid","name","email","birthdate","sex");
+    }
     std::vector <TableDataset3Data> data = tableWorker.get_normalazied_dataset3(tableDataset3);
     for (const auto& [uid,name,email,birthdate,sex] : data){
         write_dataset3(csv,uid,name,email,birthdate,sex);
